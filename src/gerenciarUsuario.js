9663a4d28b206107b9f0ad8d7d1686ea038d6df5
Matheus Viana <matheusviana762z@gmail.com>
2025-09-09 22:56:19 -0300

    alterou lista de usuarios para se tornar objetos

--- src/gerenciarUsuario.js
+++ src/gerenciarUsuario.js
@@ -1,4 +1,24 @@
-const usuarios = ['matheus', 'marcos', 'lucas', 'joão'];
+const usuarios = [
+    {
+        nome: 'Matheus',
+        email: 'matheus@matheus.com'
+    },
+
+    {
+        nome: 'Lucas',
+        email: 'lucas@lucas.com'
+
+    },
+
+    {
+        nome: 'João',
+        email: 'joao@joao.com'
+
+    }
+];
+
+
+
 
 function retornarUsuarios(){
     return usuarios;
--- test/gerenciarUsuario.test.js
+++ test/gerenciarUsuario.test.js
@@ -3,8 +3,12 @@ const { expect } = require('chai');
 
 describe('Testar as funcoes de gestao de usuarios', function () {
     it('validar que posso adicionar um novo nome de usuario na lista', function () {
-        adicionarNovoUsuario('Maria');
+        adicionarNovoUsuario({
+            nome: 'Maria',
+            email: 'maria@maria.com'
+
+        });
         const listaDeUsuarios = retornarUsuarios();
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+        expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
     });
 })
